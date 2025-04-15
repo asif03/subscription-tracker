2de5b38f6f512844a9ddddc0a336a268bf95d719
asif03 <babu033045@gmail.com>
2025-04-15 16:46:38 +0600

    Update Code

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { createSubscription } from "../controllers/subscription.controller.js";
+import auth from "../middlewares/auth.middleware.js";
 
 const subscriptionRouter = Router();
 
@@ -10,9 +12,7 @@ subscriptionRouter.get("/:id", (req, res) => {
   res.send("Subscription details route");
 });
 
-subscriptionRouter.post("/", (req, res) => {
-  res.send("Create subscription route");
-});
+subscriptionRouter.post("/", auth, createSubscription);
 
 subscriptionRouter.put("/:id", (req, res) => {
   res.send("Edit subscription route");
